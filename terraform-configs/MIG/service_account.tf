d05e4086e975bbecad23289322d339b1ec3ea69c
Eddi Sanchez <eddi@heyeddi.com>
2025-04-01 04:40:39 -0600

    Setup Service  Account with needed roles
    - cloud SQL client
    - Secret Accesor
    - Artifact Reader

--- terraform-configs/MIG/service_account.tf
+++ terraform-configs/MIG/service_account.tf
@@ -1,11 +1,23 @@
 resource "google_service_account" "default" {
-  project = data.google_project.default.project_id
+  project      = data.google_project.default.project_id
   account_id   = "mig-sa"
   display_name = "MIG Service Account"
 }
 
 resource "google_project_iam_member" "sql_user_login" {
   project = data.google_project.default.project_id
-  role    = "roles/cloudsql.instanceUser"
+  role    = "roles/cloudsql.client"
+  member  = google_service_account.default.member
+}
+
+resource "google_project_iam_member" "secret_accessor" {
+  project = data.google_project.default.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = google_service_account.default.member
+}
+
+resource "google_project_iam_member" "artifact_reader" {
+  project = data.google_project.default.project_id
+  role    = "roles/artifactregistry.reader"
   member  = google_service_account.default.member
 }
